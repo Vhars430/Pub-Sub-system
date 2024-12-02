7833cfb24e8675f0fc26b523fd52f12474ef36fc
Prabaljit Walia <prabaljitwalia4546@gmail.com>
2024-12-01 17:05:21 -0800

    cleanup

--- test/gossip.test.js
+++ test/gossip.test.js
@@ -13,10 +13,8 @@ describe('Gossip Protocol Tests', () => {
       topic: 'test-topic'
     };
     
-    // Stub the createTopicIfNotExists function
     sinon.stub(Node.prototype, 'startListening').resolves();
     
-    // Create test nodes with mocked config
     node1 = new Node('node1', mockKafkaConfig.kafkaBroker, mockKafkaConfig.topic);
     node2 = new Node('node2', mockKafkaConfig.kafkaBroker, mockKafkaConfig.topic);
     
@@ -43,7 +41,6 @@ describe('Gossip Protocol Tests', () => {
       timestamp: Date.now()
     });
 
-    // Verify state propagation - check the value property
     const node2State = gossip2.state.get('testKey');
     expect(node2State.value).to.equal(testValue);
   });
@@ -53,17 +50,13 @@ describe('Gossip Protocol Tests', () => {
     const oldValue = 'oldValue';
     const newValue = 'newValue';
     
-    // Set initial state with old version
     gossip1.updateState('key1', oldValue);
     
-    // Set newer state in gossip2
     gossip2.version = gossip1.version + 1;
     gossip2.updateState('key1', newValue);
     
-    // Try to merge older state into newer
     gossip2.mergeState(Object.fromEntries(gossip1.state));
     
-    // Should keep newer value
     const finalState = gossip2.state.get('key1');
     expect(finalState.value).to.equal(newValue);
   });
@@ -78,7 +71,7 @@ describe('Gossip Protocol Tests', () => {
     expect(publishStub.calledOnce).to.be.true;
   });
 
-  // Simple State Validation Test (replacing the invalid state test)
+  //  State Validation Test
   it('should maintain state consistency when updating', () => {
     // Update state
     gossip1.updateState('testKey', 'testValue');
@@ -86,7 +79,6 @@ describe('Gossip Protocol Tests', () => {
     // Verify state structure
     const state = gossip1.state.get('testKey');
     
-    // Check basic state properties
     expect(state).to.have.property('value', 'testValue');
     expect(state).to.have.property('version').that.is.a('number');
     expect(state).to.have.property('timestamp').that.is.a('number');
